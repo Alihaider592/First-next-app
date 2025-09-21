416e04e7c3904338e8ac12ad2516c43a8781b7d1
Malik Ali <alicoder592@gmail.com>
2025-09-21 18:45:06 +0500

    some changes added

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import { JSX } from "react";
+
+export default function page(){
+  return <h1>Hello Next.js </h1>;
+}
+// app/blog/page.tsx
+// export default function Page(): JSX.Element {
+//   return <h1 className="text-2xl font-bold">Hello Blog ✅</h1>;
+// }
