6a5a98ec3ac293c98104dddd7ce2f015a4b2cdef
Malik Ali <alicoder592@gmail.com>
2025-09-22 16:15:21 +0500

    text-correction

--- app/label/[labelId]/page.tsx
+++ app/label/[labelId]/page.tsx
@@ -2,7 +2,7 @@
 export default function Page({ params }: { params: { labelId: string } }) {
   return (
     <div>
-      <h1>this is my label Id = {params.labelId}</h1>
+      <h1> my label Id is = {params.labelId}</h1>
     </div>
   );
 }
